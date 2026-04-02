46bf965b6a05ba518f1897a9bbb4ecfdc7e20f20
Elvin Garayev <elvingarayev90@gmail.com>
2026-04-02 13:45:14 +0400

    Add Services

--- src/main/java/az/edu/ada/wm2/lab6/repository/CategoryRepository.java
+++ src/main/java/az/edu/ada/wm2/lab6/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package az.edu.ada.wm2.lab6.repository;
 
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import az.edu.ada.wm2.lab6.model.Product;
 
-public interface CategoryRepository  extends JpaRepository<Product, UUID> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import az.edu.ada.wm2.lab6.model.Category;
+
+public interface CategoryRepository  extends JpaRepository<Category, UUID> {
     
 }
